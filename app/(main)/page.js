ab6b7db690d7d5f585c475ba64ada59fe856a0b5
Majedul Hasan <hasanmajedul@gmail.com>
2024-10-02 11:31:34 +0600

    home page page

---
+++ app/(main)/page.js
@@ -0,0 +1,13 @@
+import MainNav from '@/components/header';
+import NavigationMenuDemo from '@/components/menu';
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <>
+      <div className='container flex h-20 items-center justify-between py-6 '>
+        home page
+      </div>
+    </>
+  );
+}
