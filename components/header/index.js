a2003995c279195b9ae021c5749c1377034a3b4b
Majedul Hasan <hasanmajedul@gmail.com>
2024-10-02 11:20:59 +0600

    MainNav  component

---
+++ components/header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Logo } from '../Logo';
+
+const MainNav = () => {
+  return (
+    <div className='flex '>
+      <Logo />
+    </div>
+  );
+};
+
+export default MainNav;
