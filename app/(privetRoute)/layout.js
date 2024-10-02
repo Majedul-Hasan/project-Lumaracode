2d286a67d149f140296c530e265301277d860dc2
Majedul Hasan <hasanmajedul@gmail.com>
2024-10-03 01:58:49 +0600

    page dashboard

---
+++ app/(privetRoute)/layout.js
@@ -0,0 +1,20 @@
+import MainNav from '@/components/header';
+import React from 'react';
+
+const layout = ({ children }) => {
+  return (
+    <>
+      <div className='flex min-h-screen flex-col'>
+        <header className='z-40  backdrop-blur-md fixed top-0 left-0 right-0 border-b '>
+          {/* TODO: SessionProvider  */}
+          <div className='container flex  items-center justify-between py-12 '>
+            <MainNav />
+          </div>
+        </header>
+        <main className='flex-1 pt-20 flex flex-col '>{children}</main>
+      </div>
+    </>
+  );
+};
+
+export default layout;
